3ef14ca4834698783a10b8f4bc47c6e89c17aa0d
SwayamPrabha <tanusinghsingh7565@gmail.com>
2022-02-12 21:02:16 +0530

    Revert "Revert "Updates""
    This reverts commit 4c4cea21d10ecafeabeb19a0fc47a3d21b307784.

---
+++ precache-manifest.3b8932d7496b48661f98fb01a136d5b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376dc11e8c550c6896a36d7e5a495237",
+    "url": "/TextConverter%20~%20TANU/index.html"
+  },
+  {
+    "revision": "f029da68e248e73a8d29",
+    "url": "/TextConverter%20~%20TANU/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/TextConverter%20~%20TANU/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "bd81e5c6feaf63e9a711",
+    "url": "/TextConverter%20~%20TANU/static/js/3.4183067e.chunk.js"
+  },
+  {
+    "revision": "f029da68e248e73a8d29",
+    "url": "/TextConverter%20~%20TANU/static/js/main.04357fad.chunk.js"
+  },
+  {
+    "revision": "3ef3bdacafe9070f3f7a",
+    "url": "/TextConverter%20~%20TANU/static/js/runtime~main.6a97cec1.js"
+  }
+]);
